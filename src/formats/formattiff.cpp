43665c52869a9a256a5db3ac4ef532939c4b0833
andreyu <andreyu@3ae4a26e-3d8d-4515-af9a-f071fbd64532>
2010-08-31 05:38:46 +0000

    - reading multipage TIFF ability has been added; - show real bpp of TIFF image;
    git-svn-id: svn://svn.code.sf.net/p/simpleviewer/code@65 3ae4a26e-3d8d-4515-af9a-f071fbd64532

--- src/formats/formattiff.cpp
+++ src/formats/formattiff.cpp
@@ -22,44 +22,47 @@ bool CFormatTiff::Load(const char* filename, int subImage) {
 	}
 	fclose(m_file);
 
+	bool ret	= false;
+
 	TIFF* tif	= TIFFOpen(filename, "r");
     if(tif != 0) {
-        TIFFRGBAImage img;
-        if(TIFFRGBAImageBegin(&img, tif, 0, NULL)) {
-			m_width		= img.width;
-			m_height	= img.height;
-			m_pitch		= m_width * sizeof(uint32);
-			m_sizeMem	= m_pitch * m_height;
-			m_bitmap	= new unsigned char[m_sizeMem];
-			m_bpp		= 32;
-			m_bppImage	= img.bitspersample * img.samplesperpixel;
-			m_format	= GL_RGBA;
-			m_subCount	= TIFFNumberOfDirectories(tif);
-			std::cout << " pages count: " << m_subCount;
-			m_subImage	= std::min(subImage, m_subCount - 1);
-			TIFFSetDirectory(tif, m_subImage);
+		m_subCount	= TIFFNumberOfDirectories(tif);
+		m_subImage	= std::min(subImage, m_subCount - 1);
+		if(TIFFSetDirectory(tif, m_subImage) != 0) {
+			TIFFRGBAImage img;
+			if(TIFFRGBAImageBegin(&img, tif, 0, NULL) != 0) {
+				m_width		= img.width;
+				m_height	= img.height;
+				m_pitch		= m_width * sizeof(uint32);
+				m_sizeMem	= m_pitch * m_height;
+				m_bitmap	= new unsigned char[m_sizeMem];
+				m_bpp		= 32;
+				m_bppImage	= img.bitspersample * img.samplesperpixel;
+				m_format	= GL_RGBA;
 
-			// left-to-right? top-to-bottom
-			img.req_orientation	= 1;
-			std::cout << " row: " << img.row_offset;
-			std::cout << " col: " << img.col_offset;
+				// left-to-right? top-to-bottom
+				img.req_orientation	= ORIENTATION_TOPLEFT;
+				std::cout << " row: " << img.row_offset;
+				std::cout << " col: " << img.col_offset;
 
-			if(TIFFRGBAImageGet(&img, (uint32*)m_bitmap, m_width, m_height) == 0) {
-				reset();
-				TIFFClose(tif);
-				return false;
+				if(TIFFRGBAImageGet(&img, (uint32*)m_bitmap, m_width, m_height) != 0) {
+					ret	= true;
+				}
+				TIFFRGBAImageEnd(&img);
 			}
-            TIFFRGBAImageEnd(&img);
-        }
-		else {
-			reset();
-			TIFFClose(tif);
-			return false;
 		}
         TIFFClose(tif);
     }
 
-	return true;
+	// clean if error
+	if(ret == false) {
+		if(tif != 0) {
+			TIFFClose(tif);
+		}
+		reset();
+	}
+
+	return ret;
 }
 
 void CFormatTiff::FreeMemory() {
