39e44194c0786a91c4aedb557f0711e813e15abc
andreyu <andreyu@3ae4a26e-3d8d-4515-af9a-f071fbd64532>
2010-09-03 15:09:08 +0000

    git-svn-id: svn://svn.code.sf.net/p/simpleviewer/code@69 3ae4a26e-3d8d-4515-af9a-f071fbd64532

--- src/version.h
+++ src/version.h
@@ -10,17 +10,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 9;
-	static const long BUILD = 1662;
-	static const long REVISION = 9147;
+	static const long BUILD = 1663;
+	static const long REVISION = 9149;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4951;
-	#define RC_FILEVERSION 1,9,1662,9147
-	#define RC_FILEVERSION_STRING "1, 9, 1662, 9147\0"
-	static const char FULLVERSION_STRING[] = "1.9.1662.9147";
+	static const long BUILDS_COUNT = 4954;
+	#define RC_FILEVERSION 1,9,1663,9149
+	#define RC_FILEVERSION_STRING "1, 9, 1663, 9149\0"
+	static const char FULLVERSION_STRING[] = "1.9.1663.9149";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 1;
+	static const long BUILD_HISTORY = 2;
 	
 
 }
--- src/window.cpp
+++ src/window.cpp
@@ -239,10 +239,10 @@ void CWindow::fnMouseButtons(int button, int state, int x, int y) {
 	case GLUT_LEFT_BUTTON:
 		if(state == GLUT_DOWN) {
 			m_selection->StartPoint(x - m_imageDx, y - m_imageDy);
-			glutPostRedisplay();
+//			glutPostRedisplay();
 		}
 		else if(state == GLUT_UP) {
-			m_selection->EndPoint(x - m_imageDx, y - m_imageDy);
+//			m_selection->EndPoint(x - m_imageDx, y - m_imageDy);
 			glutPostRedisplay();
 		}
 		m_mouseLB	= (state == GLUT_DOWN);
@@ -494,7 +494,7 @@ bool CWindow::loadImage(int step, int subImage) {
 		m_na->Enable(true);
 	}
 
-//	m_selection->SetImageDimension(m_il->GetWidth(), m_il->GetHeight());
+	m_selection->SetImageDimension(m_il->GetWidth(), m_il->GetHeight());
 	updateInfobar();
 
 	fnResize(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
