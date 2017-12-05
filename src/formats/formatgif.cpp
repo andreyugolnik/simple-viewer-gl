68963cf7b930b114c85a67c4e9bb830aba06d9b5
Andrey A. Ugolnik <andrey@ugolnik.info>
2017-12-05 16:52:18 +0300

    Interlaced GIF bug fixed.

--- src/formats/formatgif.cpp
+++ src/formats/formatgif.cpp
@@ -19,7 +19,6 @@
 
 namespace
 {
-
     GifFileType* OpenFile(const char* path)
     {
 #if GIFLIB_MAJOR >= 5
@@ -57,6 +56,15 @@ namespace
         }
     }
 
+    void putRow(sBitmapDescription& desc, uint32_t row, uint32_t width, const SavedImage& image, const ColorMapObject* cmap, uint32_t transparentIdx)
+    {
+        for (uint32_t x = 0; x < width; x++)
+        {
+            const uint32_t idx = image.RasterBits[row * width + x];
+            const uint32_t pos = (row + image.ImageDesc.Top) * desc.pitch + (x + image.ImageDesc.Left) * 4;
+            putPixel(desc, pos, &cmap->Colors[idx], transparentIdx == idx);
+        }
+    }
 }
 
 cFormatGif::cFormatGif(iCallbacks* callbacks)
@@ -77,7 +85,7 @@ bool cFormatGif::isSupported(cFile& file, Buffer& buffer) const
 
     const auto h = buffer.data();
     return (h[0] == 'G' && h[1] == 'I' && h[2] == 'F' && h[3] == '8' && h[5] == 'a')
-           && (h[4] == '7' || h[4] == '9');
+        && (h[4] == '7' || h[4] == '9');
 }
 
 bool cFormatGif::LoadImpl(const char* filename, sBitmapDescription& desc)
@@ -150,16 +158,16 @@ bool cFormatGif::load(uint32_t current, sBitmapDescription& desc)
     }
 
     // look for the transparent color extension
-    uint32_t transparent = (uint32_t) - 1U;
+    uint32_t transparentIdx = (uint32_t)-1U;
     for (int i = 0; i < image.ExtensionBlockCount; i++)
     {
         const auto& eb = image.ExtensionBlocks[i];
         if (eb.Function == 0xF9 && eb.ByteCount == 4)
         {
-            bool has_transparency = ((eb.Bytes[0] & 1) == 1);
-            if (has_transparency)
+            const bool hasTransparency = (eb.Bytes[0] & 1) == 1;
+            if (hasTransparency)
             {
-                transparent = eb.Bytes[3];
+                transparentIdx = eb.Bytes[3];
             }
 
             const uint32_t disposalMode = (eb.Bytes[0] >> 2) & 0x07;
@@ -181,43 +189,44 @@ bool cFormatGif::load(uint32_t current, sBitmapDescription& desc)
     if (image.ImageDesc.Interlace)
     {
         // Need to perform 4 passes on the images:
-        const uint32_t interlacedOffset[] = { 0, 4, 2, 1 };  // The way Interlaced image should
-        const uint32_t interlacedJumps[] = { 8, 8, 4, 2 };   // be read - offsets and jumps...
-        uint32_t interlaced_y = 0;
-
-        for (uint32_t i = 0; i < 4; i++)
+        struct Interlace
         {
-            for (uint32_t y = interlacedOffset[i]; y < height; y += interlacedJumps[i])
+            uint32_t offset;
+            uint32_t jump;
+        };
+        const Interlace interlaced[] = {
+            { 0, 8 },
+            { 4, 8 },
+            { 2, 4 },
+            { 1, 2 },
+        };
+
+        uint32_t row = 0;
+
+        for (const auto& interlace : interlaced)
+        {
+            for (uint32_t y = interlace.offset; y < height; y += interlace.jump)
             {
-                for (uint32_t x = 0; x < width; x++)
-                {
-                    const uint32_t idx = image.RasterBits[interlaced_y * width + x];
-                    const uint32_t pos = (y + image.ImageDesc.Top) * desc.pitch + (x + image.ImageDesc.Left) * 4;
-                    putPixel(desc, pos, &cmap->Colors[idx], transparent == idx);
-                }
+                putRow(desc, y, width, image, cmap, transparentIdx);
 
-                updateProgress((float)interlaced_y / height);
-
-                interlaced_y++;
+                updateProgress((float)row / height);
+                row++;
             }
         }
+
+        m_formatName = "gif/i";
     }
     else
     {
         for (uint32_t y = 0; y < height; y++)
         {
-            for (uint32_t x = 0; x < width; x++)
-            {
-                const uint32_t idx = image.RasterBits[y * width + x];
-                const uint32_t pos = (y + image.ImageDesc.Top) * desc.pitch + (x + image.ImageDesc.Left) * 4;
-                putPixel(desc, pos, &cmap->Colors[idx], transparent == idx);
-            }
+            putRow(desc, y, width, image, cmap, transparentIdx);
 
             updateProgress((float)y / height);
         }
-    }
 
-    m_formatName = "gif";
+        m_formatName = "gif/p";
+    }
 
     return true;
 }
