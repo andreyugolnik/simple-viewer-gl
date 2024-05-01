0bcfdee5ff39b8d85066fddcdffdc7c50e2f1b62
Andrey Ugolnik <andrey@ugolnik.info>
2024-05-01 18:56:20 +0200

    Absent file added.

---
+++ src/version.h
@@ -0,0 +1,19 @@
+/**********************************************\
+*
+*  Simple Viewer GL edition
+*  by Andrey A. Ugolnik
+*  http://www.ugolnik.info
+*  andrey@ugolnik.info
+*
+\**********************************************/
+
+#pragma once
+
+namespace version
+{
+    const char* getTitle();
+    int getMajor();
+    int getMinor();
+    int getRelease();
+
+} // namespace version
